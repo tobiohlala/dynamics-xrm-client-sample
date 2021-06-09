8fad4997a70f7838d5ff73f20adfa6ea3b652ad1
Tobias Heilig <the@objektkultur.de>
2021-06-09 22:06:03 +0200

    Initial

---
+++ DynamicsXrmClientSample/DynamicsXrmClientSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using DynamicsXrmClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DynamicsXrmClientSample
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var client = await DynamicsXrmWebApiClient.ConnectAsync(
+                new DynamicsXrmConnectionParams
+                {
+                    ServiceRootUri = "<service-root-uri>",
+                    TenantId = "<tenant-id>",
+                    ClientId = "<client-id>",
+                    ClientSecret = "<client-secret>"
+                });
+
+            string whereEffortGreater8Hours = "?$filter=new_estimatedeffort_number gt 8";
+
+            List<SalesOrder> orders = await client.RetrieveMultipleAsync<SalesOrder>(whereEffortGreater8Hours);
+        }
+    }
+}
---
+++ DynamicsXrmClientSample/DynamicsXrmClientSample/SalesOrder.cs
@@ -0,0 +1,17 @@
+﻿using DynamicsXrmClient.Attributes;
+using System;
+using System.Text.Json.Serialization;
+
+namespace DynamicsXrmClientSample
+{
+    [DataverseTable("salesorder", "salesorders")]
+    public class SalesOrder
+    {
+        [DataverseRowId]
+        [JsonPropertyName("salesorderid")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("new_estimatedeffort_number")]
+        public int EstimatedEffort { get; set; }
+    }
+}
